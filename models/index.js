ba6d66338715b4496334b12563bb7fff3ccde543
kaihuan Huang <huanhuan@kaihuans-MBP.attlocal.net>
2022-09-13 12:15:49 -0700

    update js file in models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongTO(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany( Tag, {
+  foreignKey: 'tag_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany (Product,{
+  foreignKey:'product_id'
+})
 module.exports = {
   Product,
   Category,
