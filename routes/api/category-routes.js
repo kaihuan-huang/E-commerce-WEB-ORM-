10d60f62ce16233b1f3d0d45cbb061b27568e5e8
kaihuan Huang <huanhuan@kaihuans-MBP.attlocal.net>
2022-09-13 12:41:54 -0700

    update category api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,77 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all categories
+  try{
+    const categoriesData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoriesData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
+  try{
+    const categorieData = await Category.findByPk(req.params.id {
+      include: [{ model: Product }],
+    });
+    if (!categorieData) {
+      res.status(404).json({ message: ' No Product with that id!'});
+      return;
+    }
+
+    res.status(200).json(categorieData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create( {
+      product_id : req.params.product_id,
+    });
+    res.status(200).json(newCategory);
+  }catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update( {
+      product_id : req.params.product_id,
+    });
+    res.status(200).json(updateCategory);
+  }catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(deleteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
